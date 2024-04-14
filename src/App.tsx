69a56e076ea0295d578c23c1cc1bfbf8b5e97d9a
saetgit <etemadian.sara.1998@gmail.com>
2024-04-14 15:29:12 +0330

    add type as object ans array global and export and import that

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
-
+type User2={name:string;age:number}
+export type Array2={num1:number}
 function App() {
 
   let name :string= "ali"
   name="1"
 
-  ////////////////////////////////////////////////
+  ////////////////////////////////////////////////Type for object and array
   let array:number[]=[1,2,3]
   let array2:(number|string)[]=[1,"sara"]
 
-  let user:{name:string;age:number}={
+  let user:User2={
     name:"sara",
     age:25
   }
